a7d312a74e6ad8959849e8fe2b4599292f3397ea
Raven Code <soulidbrasil@gmail.com>
2020-02-15 14:18:47 -0300

    added comments to vectors.dart

--- example/vectors.dart
+++ example/vectors.dart
@@ -1,11 +1,46 @@
+// Copyright (c) 2020, Raven Code.  MIT LICENSE
+// @dart = 2.7
+
+/**
+ * Author: Raven Code
+ * 
+ *
+ * Math for game developers
+ * 
+ * 
+ * 
+ * 
+ *
+ * 
+ *
+ * 
+ */
+
+// How to use vector to move objects on games and animation.
+// The main function of vectors in game is control velocity and direction.
+// We can use it to determine the distance between elements. (Vector length)
+//
+// Inversion of vectors
+//
+// We can invert a vector in the simple way, where is head becames tail and tail becames head
+// To do that  in programming we just add -1  to the vector.
+
 import 'dart:html';
 
+// Create a Canvas and added it to the DOM  and get 2D context
+// ths we will use their methods to draw on canvas.
 var canvas = CanvasElement();
 var context2D = canvas.context2D;
 
+// Define a start Point for our box, x and y.
+// Our velocity and direction will be set by a vector, boxVelocity, x and y.
+// x positive thes head is to the RIGTH, negativa to the LEFT
+// the value inside x detemine the velocity.
 var boxPosition = Point(0, 0);
 var boxVelocity = Vector(3, 1);
 
+// Start the main loop of animation / game
+// call update, draw and loop again.
 run() async {
   await window.requestAnimationFrame(loop);
 }
@@ -16,6 +51,22 @@ loop(num timestamp) {
   run();
 }
 
+// The vector is added to our boxPositon, a Point(x,y),
+//
+// boxPosition and boxVelocity (vector) details:
+//
+// boxPosition.x = 0;
+// boxPositon.y = 0;
+//
+// boxVelocity.x = 3;
+// boxVelocity.y = 1;
+//
+//  This operation : boxPosition += boxVelocity;
+// to do that:
+// boxPosition.x =  boxPosition.x  + boxVelocity.x
+//             3                0  +             3
+// boxPosition.y =  boxPosition.y  + boxVelocity.y
+//             1                0  +             1
 update() {
   boxPosition += boxVelocity;
 
@@ -42,6 +93,10 @@ class Point {
   double x, y;
   Point(this.x, this.y);
 
+  // Operator Overlay
+  // Allow us change an operato like ===, or, + , -
+  // Here we change the minus to allow it adde a Vector to the current point
+  // The x and y the will be adde to x and y to the point and will be returned a new point.
   Point operator -(Vector vector) {
     Point potision = Point(0, 0);
 
@@ -52,12 +107,12 @@ class Point {
   }
 
   Point operator +(Vector vector) {
-    Point potision = Point(0, 0);
+    Point position = Point(0, 0);
 
-    potision.x = x + vector.x;
-    potision.y = y + vector.y;
+    position.x = x + vector.x;
+    position.y = y + vector.y;
 
-    return potision;
+    return position;
   }
 }
 
